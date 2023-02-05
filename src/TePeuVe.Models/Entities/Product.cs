22cd3e8550dc5afd8e5e936f16d2df8e6fe3c7d1
Rodrigo Moya <rodrigo@gnome.org>
2023-02-05 19:08:02 +0100

    Add .Models project

---
+++ src/TePeuVe.Models/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TePeuVe.Models.Entities
+{
+	public class Product
+	{
+		public string Name { get; set; }
+		public double UnitPrice { get; set; }
+	}
+}
+
---
+++ src/TePeuVe.Models/Entities/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TePeuVe.Models.Entities
+{
+	public class Sale
+	{
+		public int Id { get; internal set; }
+		public DateTime Date { get; internal set; }
+		public List<SaleItem> Items { get; } = new();
+	}
+
+	public class SaleItem
+	{
+		public Product Product { get; set; }
+		public double Quantity { get; set; }
+	}
+}
+
